b90fc01189976cda72047eb7555e223db640d971
Vinay0726 <vinaythaware143@gmail.com>
2024-08-06 18:10:01 +0530

    Added About us section

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import { AiOutlineClose } from "react-icons/ai";
+
+function AboutMe({close, setClose}) {
+  
+
+  return (
+    <div
+      className={` h-[90%] w-[75%]  flex top-10 left-52 shadow-2xl bg-transparent backdrop-blur-3xl absolute z-10 ${
+        close ? "hidden" : "block"
+      }`}
+    >
+      <AiOutlineClose
+        className="h-[30px] w-[30px] bg-slate-100 absolute right-5 top-5 pointer-events-auto"
+        onClick={() => setClose(true)}
+      />
+
+      <div className="w-[60%] h-full">
+        <div className="ml-40 h-[55%] w-full flex flex-col justify-center items-start gap-5">
+          <h1 className="text-blue-600 text-2xl font-bold">ABOUT ME</h1>
+          <p className="w-[60%] font-semibold">
+            I help business owners and busy web developers to design and develop
+            creative websites that fit their vision and attract visitors to stay
+            forever. Technologies and tools that I use to create such awesome
+            websites include my expertise in web development and fullstack
+            development.
+          </p>
+          <div className="w-[55%]">
+            <button
+              type="button"
+              className="text-gray-500 bg-transparent border border-gray-800 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-xl text-sm px-5 py-1.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            >
+              #react.js
+            </button>
+            <button
+              type="button"
+              className="text-gray-500 bg-transparent border border-gray-800 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-xl text-sm px-5 py-1.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            >
+              #springboot
+            </button>
+            <button
+              type="button"
+              className="text-gray-500 bg-transparent border border-gray-800 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-xl text-sm px-5 py-1.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            >
+              #html
+            </button>
+            <button
+              type="button"
+              className="text-gray-500 bg-transparent border border-gray-800 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-xl text-sm px-5 py-1.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            >
+              #css
+            </button>
+            <button
+              type="button"
+              className="text-gray-500 bg-transparent border border-gray-800 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-xl text-sm px-5 py-1.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            >
+              #javascript
+            </button>
+            <button
+              type="button"
+              className="text-gray-500 bg-transparent border border-gray-800 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-xl text-sm px-5 py-1.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            >
+              #tailwind
+            </button>
+            <button
+              type="button"
+              className="text-gray-500 bg-transparent border border-gray-800 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-xl text-sm px-5 py-1.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            >
+              #bootstrap
+            </button>
+            <button
+              type="button"
+              className="text-gray-500 bg-transparent border border-gray-800 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-xl text-sm px-5 py-1.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            >
+              #java
+            </button>
+            <button
+              type="button"
+              className="text-gray-500 bg-transparent border border-gray-800 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-xl text-sm px-5 py-1.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            >
+              #sql
+            </button>
+            <button
+              type="button"
+              className="text-gray-500 bg-transparent border border-gray-800 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-xl text-sm px-5 py-1.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            >
+              #github
+            </button>
+          </div>
+        </div>
+        <div className=" h-[45%] w-full">
+          <h1 className="ml-40 text-blue-600 text-2xl font-bold">FULL STACK</h1>
+          <div className="flex gap-5 w-[60%] ml-40 mt-10">
+            <div className="flex flex-col justify-center items-center">
+              <img src="/reactjs.svg" className="h-[80px] w-[80px]" alt="" />
+              <p className="text-blue-300 font-semibold">React Js</p>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <img src="/springboot.svg" alt="" className="h-[80px] w-[80px]" />
+              <p className="text-green-300 font-semibold">SpringBoot</p>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <img src="/mysql.svg" alt="" className="h-[80px] w-[80px]" />
+              <p className="text-orange-400 font-semibold">MySQL</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-[40%] h-full flex justify-center items-center border-gray-60000">
+        <img src="/coder.svg" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default AboutMe;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {motion} from 'framer-motion'
+import AboutMe from './AboutMe';
 
 function Hero() {
+  const [close,setClose]=useState(true);
   return (
     <div className="w-[100%] md:h-full md:w-screen">
-      <div className="h-96 ml-5 mt-[120%] md:w-[400px] md:h-80 relative -z-0 flex flex-col justify-center items-start gap-6  md:mt-[10%] md:ml-[20%]">
+      <div className="h-96 ml-5 duration-300 mt-[120%] md:w-[400px] md:h-80 relative -z-0 flex flex-col justify-center items-start gap-6  md:mt-[10%] md:ml-[20%]">
         <div className="text-8xl md:text-6xl font-bold font-serif text-black">
           Vinay Thaware
         </div>
@@ -15,7 +17,7 @@ function Hero() {
           <button
             type="button"
             class="pointer-events-auto text-white flex justify-center items-center h-12 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 "
-          >
+          onClick={()=>setClose(false)}>
             About Me
           </button>
           <button
@@ -27,12 +29,7 @@ function Hero() {
         </div>
       </div>
       <div className="-left-20 -z-10 top-24 h-[600px] w-[600px] md:h-[650px]  md:w-[650px] absolute md:z-0 md:top-2 md:left-[40%] pointer-events-none">
-        <img
-          src="/logo.png"
-          className="w-full h-full neon-blue"
-          alt=""
-         
-        />
+        <img src="/logo.png" className="w-full h-full neon-blue" alt="" />
       </div>
       <div className="flex top-40  absolute bg-white pointer-events-none md:top-52 md:right-5  h-[312px] w-[64px] flex-col justify-center items-center gap-5 ">
         <motion.img
@@ -76,6 +73,8 @@ function Hero() {
           alt=""
         />
       </div>
+
+      <AboutMe close={close} setClose={setClose} />
     </div>
   );
 }
